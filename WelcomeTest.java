de49cb56460b1ea630f455e601b0ec8ded5936a5
mislam108 <mislam108@lamar.edu>
2024-06-01 23:06:40 -0500

    Initial commit

---
+++ WelcomeTest.java
@@ -0,0 +1,29 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.junit.Assert.assertEquals;
+
+public class WelcomeTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void testMain() {
+        Welcome.main(new String[0]);
+        String expectedOutput = "Welcome to Java!\nWelcome to Java2!\n";
+        assertEquals(expectedOutput, outContent.toString());
+    }
+}
